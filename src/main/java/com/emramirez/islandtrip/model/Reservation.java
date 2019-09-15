a18b4e2687e3f888450b66475fa5c3624dc165e3
ramsysproject <m.ramirez05@gmail.com>
2019-09-15 18:46:49 -0300

    Add table annotations

--- src/main/java/com/emramirez/islandtrip/model/Reservation.java
+++ src/main/java/com/emramirez/islandtrip/model/Reservation.java
@@ -2,13 +2,24 @@ package com.emramirez.islandtrip.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
+@Entity
 public class Reservation {
+    @Id
     private Long id;
+
+    @Column
     private UUID uuid;
+
+    @Column
     private LocalDate startingDate;
+
+    @Column
     private LocalDate endingDate;
 }
