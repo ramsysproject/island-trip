be54009b2032922723099048cd27b832b4b762af
ramsysproject <m.ramirez05@gmail.com>
2019-09-15 17:24:03 -0300

    Project initialization

---
+++ src/main/java/com/emramirez/islandtrip/Application.java
@@ -0,0 +1,13 @@
+package com.emramirez.islandtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
